e2f27c916db53872174f9bfbfff2974b05d17147
Breno Lira <brenoliradev@gmail.com>
2023-06-09 14:19:14 -0300

    feat: support for edit/create/toggle ready

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -4,22 +4,42 @@ import { TodoStore } from '../composable/TodoStore'
 </script>
 
 <template>
-  <main class="flex flex-col items-center justify-center gap-4 w-11/12 mx-auto">
+  <main
+    class="mx-auto flex w-full flex-col items-center justify-center gap-4 px-3 lg:w-11/12"
+  >
     <section
-      class="flex max-w-[1980px] items-center justify-center px-4 py-6 text-white"
+      class="flex max-w-[1980px] flex-col items-center justify-center px-4 py-6 text-white"
     >
       <h2 class="text-4xl font-semibold">Vue to-do-list</h2>
     </section>
-    <form @submit="submitForm">
+    <form
+      class="flex flex-col items-center justify-center"
+      @submit="submitForm"
+    >
       <div class="join">
         <input
+          name="create-todo"
           type="text"
           v-model="text"
-          class="input-bordered input join-item"
+          class="input-bordered input-secondary input join-item"
           placeholder="Insert a todo"
+          autofocus
+          required
         />
-        <button :disabled="!text" class="join-item btn rounded-r-full disabled:cursor-not-allowed" type="submit">Add</button>
+        <button
+          :disabled="!text || text.length > 50"
+          class="btn-secondary join-item btn rounded-r-full disabled:cursor-not-allowed"
+          type="submit"
+        >
+          Add
+        </button>
       </div>
+      <label
+        :data-invalid="text.length > 50"
+        for="create-todo"
+        class="data-[invalid='true']:label data-[invalid='true']:label-text data-[invalid='false']:hidden data-[invalid='true']:text-red-600"
+        >The maximum size of an todo name is 50 chars.</label
+      >
     </form>
     <transition-group
       tag="ul"
@@ -29,9 +49,13 @@ import { TodoStore } from '../composable/TodoStore'
       leave-active-class="duration-200 ease-in"
       leave-from-class="opacity-100"
       leave-to-class="transform opacity-0"
-      class="flex w-full flex-col space-y-4"
+      class="flex w-full flex-col items-center justify-center space-y-4"
     >
-      <ToDoListItem v-for="todo in TodoStore.todos" :key="todo.id" :todo="todo" />
+      <ToDoListItem
+        v-for="todo in TodoStore.todos"
+        :key="todo.id"
+        :todo="todo"
+      />
     </transition-group>
   </main>
 </template>
--- src/components/ToDoListItem.vue
+++ src/components/ToDoListItem.vue
@@ -5,41 +5,133 @@ import { TodoStore } from '../composable/TodoStore'
 const props = defineProps<{
   todo: Todo
 }>()
-
 </script>
 
 <template>
   <li
-    :data-checked="props.todo.done"
-    class="flex w-full items-center justify-between rounded-2xl bg-primary-content px-6 py-4 font-medium text-white transition-all duration-300 data-[checked='true']:opacity-80"
+    :data-done="props.todo.done"
+    :data-edit="edit"
+    class="flex w-full max-w-xl flex-col items-center justify-between gap-4 rounded-2xl border border-secondary bg-transparent px-4 py-4 font-medium text-white shadow-md shadow-secondary/10 transition-all duration-300 data-[done='true']:opacity-80 sm:px-6 lg:flex-row"
   >
-    <p
-      :data-checked="props.todo.done"
-      class="text-sm text-primary-focus data-[checked='true']:italic data-[checked='true']:line-through"
+    <label
+      class="label"
+      :for="'toggle done for todo with id: ' + props.todo.id"
     >
-      {{ props.todo.text }}
-    </p>
-    <div class="flex gap-3"><input
-      type="checkbox"
+      <input
+        type="checkbox"
+        :name="'toggle done for todo with id: ' + props.todo.id"
+        :checked="props.todo.done"
+        :disabled="edit"
+        class="checkbox-secondary checkbox"
+        @change="TodoStore.toggleDoneTodo(props.todo.id)"
+      />
+    </label>
+    <button
       :name="'toggle done for todo with id: ' + props.todo.id"
-      :checked="props.todo.done"
-      class="checkbox-secondary checkbox"
-      @change="TodoStore.toggleDoneTodo(props.todo.id)"
-    />
-    <button       :name="'delete todo with id: ' + props.todo.id"
->
-      <TrashIcon @click="TodoStore.deleteTodo(props.todo.id)" class="h-6 w-6 fill-secondary hover:fill-secondary-focus hover:opacity-80 cursor-pointer"/>
+      @click="TodoStore.toggleDoneTodo(props.todo.id)"
+    >
+      <p
+        :data-done="props.todo.done"
+        :data-edit="edit"
+        class="md:text-md cursor-pointer text-sm text-primary-content data-[done='true']:italic data-[done='true']:line-through"
+        v-if="!edit"
+      >
+        {{ props.todo.text }}
+      </p>
     </button>
-  </div>
+    <form
+      :data-done="props.todo.done"
+      :data-edit="edit"
+      v-if="edit"
+      @submit="(e) => submitForm(e, props.todo.id)"
+      class="flex flex-col items-center justify-center"
+    >
+      <div class="join">
+        <input
+          :id="'edit-todo ' + props.todo.id"
+          name="edit-todo"
+          type="text"
+          v-model="newText"
+          class="input-bordered input-secondary input join-item"
+          placeholder="Edit your todo"
+          required
+        />
+        <button
+          :name="'edit todo with id: ' + props.todo.id"
+          :disabled="!newText || newText.length > 50"
+          class="join-item btn rounded-r-full disabled:cursor-not-allowed"
+          type="submit"
+        >
+          Edit
+        </button>
+      </div>
+      <label
+        :data-invalid="newText.length > 50"
+        for="edit-todo"
+        class="data-[invalid='true']:label data-[invalid='true']:label-text data-[invalid='false']:hidden data-[invalid='true']:text-red-600"
+        >The maximum size of an todo name is 50 chars.</label
+      >
+    </form>
+    <div class="flex gap-3">
+      <button
+        @click="TodoStore.deleteTodo(props.todo.id)"
+        :name="'delete todo with id: ' + props.todo.id"
+      >
+        <TrashIcon
+          class="h-6 w-6 cursor-pointer fill-secondary hover:fill-secondary-focus hover:opacity-80"
+        />
+      </button>
+      <button
+        @click="toggleEdit(props.todo.id)"
+        :name="'delete todo with id: ' + props.todo.id"
+      >
+        <PencilSquareIcon
+          class="h-6 w-6 cursor-pointer fill-secondary hover:fill-secondary-focus hover:opacity-80"
+        />
+      </button>
+    </div>
   </li>
 </template>
 
 <script lang="ts">
-import { TrashIcon } from "@heroicons/vue/20/solid"
+import { TrashIcon } from '@heroicons/vue/20/solid'
+import { PencilSquareIcon } from '@heroicons/vue/20/solid'
+import { nextTick } from 'vue'
+
+type ToDoListData = {
+  newText: string
+  edit: boolean
+}
 
 export default {
+  data(): ToDoListData {
+    return {
+      newText: '',
+      edit: false
+    }
+  },
   components: {
-    TrashIcon
+    TrashIcon,
+    PencilSquareIcon
+  },
+  methods: {
+    submitForm(event: Event, id: number) {
+      event.preventDefault() // Prevent the default form submission
+
+      if (this.newText === '') return
+      TodoStore.updateTodo(id, this.newText)
+
+      // Reset the form fields
+      this.newText = ''
+      this.edit = false
+    },
+    toggleEdit(id: number) {
+      this.edit = !this.edit
+
+      nextTick(() => {
+        document.getElementById('edit-todo ' + id)?.focus()
+      })
+    }
   }
 }
 </script>
